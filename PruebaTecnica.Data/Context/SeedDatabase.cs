6b0205b0f3d64f256bdd56787870084f9d769b89
Jhon Ander Padilla Pinto <padilla102@hotmail.com>
2019-10-11 03:06:48 -0500

    Se implementa la capa de datos

---
+++ PruebaTecnica.Data/Context/SeedDatabase.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using PruebaTecnica.Data.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PruebaTecnica.Data.Context
+{
+    public static class SeedDatabase
+    {
+        public static void Initialize(IServiceProvider serviceProvider)
+        {
+            var context = serviceProvider.GetRequiredService<Context>();
+
+            context.Database.EnsureCreated();
+
+            if (!context.Region.Any())
+            {
+                Municipio municipio = new Municipio()
+                {
+                    Name = "Medellín",
+                    status = 1
+                };
+
+                Region region = new Region()
+                {
+                    Municipios = new List<Municipio> {municipio},
+                    Name = "Andina"
+                };
+
+                context.SaveChangesAsync(); 
+            }
+
+
+        }
+    }
+}
